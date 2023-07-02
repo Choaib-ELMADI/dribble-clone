58727efeb7e16e7e22660c60d69b2cd0960bc582
Choaib ELMADI <choaib3elmadi@gmail.com>
2023-07-02 18:12:38 +0100

    Create the models

--- app/layout.tsx
+++ app/layout.tsx
@@ -8,7 +8,7 @@ export const metadata = {
   title: 'Flexibble',
   description: 'Dribble clone, showcase and discover projects',
 };
-  
+
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
